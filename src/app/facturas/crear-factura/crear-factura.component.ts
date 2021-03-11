e282303e61b01f542d8f0560eb9d645d53c02675
ang-sant-marzo <ang.sant.marzo@gmail.com>
2021-03-11 12:24:53 +0000

    validator propio

--- src/app/facturas/crear-factura/crear-factura.component.ts
+++ src/app/facturas/crear-factura/crear-factura.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ControlContainer, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ValidateCif } from 'src/app/validators/cif.validator';
 
 @Component({
   selector: 'app-crear-factura',
@@ -15,9 +16,12 @@ export class CrearFacturaComponent implements OnInit {
   ngOnInit(): void {
     this.formFra = new FormGroup({
       cliente: new FormControl('', [Validators.required, Validators.minLength(4)]),
-      cif: new FormControl(''),
+      cif: new FormControl('', [ValidateCif]),
       fecha: new FormControl((new Date()).toISOString().substring(0,10)),
-      baseImponible: new FormControl(0)
+      baseImponible: new FormControl(0),
+      tipoIVA: new FormControl(0.21),
+      importeIVA: new FormControl(0),
+      totalFra: new FormControl(0)
     })
   }
 
